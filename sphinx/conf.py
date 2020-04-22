16d519ee7ebd1bb1fba6328f979e86e8a7f5fae8
sbeniamine <beniamine@shh.mpg.de>
2020-04-22 15:25:21 +0200

    Docs: Napoleon is now part of sphinx (#2)

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -40,7 +40,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 napoleon_include_special_with_doc = False
