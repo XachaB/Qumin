aa9c0c04c1aa560688eb4c8f3c83e57b35871052
sbeniamine <beniamine@shh.mpg.de>
2020-06-08 11:31:51 +0200

    Insertion cost lowered

--- bin/find_patterns.py
+++ bin/find_patterns.py
@@ -165,7 +165,7 @@ if __name__ == '__main__':
 
     parser.add_argument("--gap_proportion",
                         help="Proportion of the median similarity cost assigned to the insertion cost.",
-                        type=float, default=.49)
+                        type=float, default=.24)
 
     parser.add_argument("--optim_mem",
                         help="Attempt to optimize RAM usage",
