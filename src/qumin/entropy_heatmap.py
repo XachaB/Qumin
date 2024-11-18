40bf6540d5edfa3a1c4f17f9abbb43b224c7e898
jbout <jules.bouton@u-paris.fr>
2024-11-18 15:24:23 +0200

    make heatmap compatible with P(success) and add n_pairs option

--- src/qumin/entropy_heatmap.py
+++ src/qumin/entropy_heatmap.py
@@ -48,23 +48,93 @@ def get_features_order(features_file, results, sort_order=False):
             return sorted(list(df.predictor.unique()))
 
 
-def entropy_heatmap(results, md, cmap_name=False,
-                    feat_order=None, dense=False, annotate=False,
-                    parameter=False):
+def _draw_heatmap(*args, cmap=None, feat_order=None, dense=False, **kwargs):
+    """ Draws a heatmap in a FacetGrid with custom parameters
 
+    Arguments:
+        feat_order (List[str]): an ordered list of each cell name.
+            Used to sort the labels.
+    """
+
+    df = kwargs.pop('data')
+    annot = kwargs.pop('annotate')
+
+    types = df["type"].unique()
+    df.index.name = 'predictor'
+    df.columns.name = 'predicted'
+    reverse = df.reverse.any()
+    df = df.pivot(index=args[0], columns=args[1], values=args[2])
+
+    # For n_pairs, we want a specific set of parameters.
+    if 'Number of pairs' in types:
+        hm_cmap = "gray_r"
+        annot = df.shape[0] < 10
+        fmt = ".0f"
+    else:
+        hm_cmap = cmap
+        fmt = ".2f"
+    if reverse:
+        hm_cmap = hm_cmap.reversed()
+
+    if feat_order:
+
+        # Sorting for multiple predictors.
+        def sorting(x):
+            return x.apply(lambda cell: feat_order.index(cell))
+
+        sort = df.index.to_frame().predictor.str.split('&', expand=True)
+        sort = sort.sort_values(by=list(sort.columns), key=sorting, axis=0)
+        df = df.reindex(sort.index)
+        df = df[feat_order]
+
+    else:
+        df = df.reindex(list(df.columns))
+
+    df = df.replace([np.nan], 0)
+
+    # Additional options for rendering.
+    # Extra short labels
+    if dense:
+        def shorten(x):
+            return ".".join([f[0].capitalize() for f in x.split('.')])
+
+        df.index = pd.Index(df.index.to_series().map(shorten))
+        df.columns = pd.Index(df.columns.to_series().map(shorten))
+
+    # Annotations on the heatmap
+    if annot:
+        annot = df.copy().round(2)
+
+    # Mask (diagonal)
+    df_m = pd.DataFrame(columns=df.columns, index=df.index)
+    for col in df.columns:
+        tmp = df_m.index.to_frame().predictor.str.split('&').apply(lambda x: col in x)
+        df_m.loc[tmp, [col]] = True
+
+    # Drawing each individual heatmap
+    sns.heatmap(df,
+                annot=annot,
+                mask=df_m,
+                cmap=hm_cmap,
+                fmt=fmt,
+                linewidths=1,
+                **kwargs)
+
+
+def entropy_heatmap(results, md, cmap_name=False,
+                    dense=False, annotate=False,
+                    n_pairs=False, **kwargs):
     """Make a FacetGrid heatmap of all metrics.
 
     Arguments:
-        parameter: ## What is this ? ##
         results (:class:`pandas:pandas.DataFrame`):
             a results DataFrame as produced by calc_paradigm_entropy.
         md (qumin.utils.Metadata): MetaData handler to get access to file location.
         cmap_name (str): name of the cmap to use. Defaults to the following cubehelix
             map, `sns.cubehelix_palette(start=2, rot=0.5, dark=0, light=1, as_cmap=True)`.
-        feat_order (List[str]): an ordered list of each cell name.
-            Used to sort the labels.
         dense (bool): whether to use short cell names or not.
         annotate (bool): whether to add an annotation overlay.
+        n_pairs (bool): whether to display a heatmap of the number of pairs.
     """
 
     if not cmap_name:
@@ -79,89 +149,44 @@ def entropy_heatmap(results, md, cmap_name=False,
                              ).stack().reset_index()
 
     df.rename(columns={"level_4": "type", 0: "value"}, inplace=True)
+
+    if not n_pairs:
+        df.drop(df[df.type == "n_pairs"].index, axis=0, inplace=True)
+        orient_param = dict(col="measure")
+    else:
+        orient_param = dict(row="measure", col="type")
+
+    # Check if we need to reverse the colormap
+    df.loc[:, 'reverse'] = df.measure.str.contains('accuracy') & (df.type == "value")
     df.loc[:, 'type'] = df.type.replace({'value': 'Result', 'n_pairs': 'Number of pairs'})
-    df.loc[:, 'measure'] = df.measure.replace({'cond_entropy': 'Conditional entropy'})
+
+    def rename(x):
+        names = {'cond_entropy': 'H(pattern|classes)',
+                 'accuracy': 'P(success|classes)'}
+        suffix = " (debug)" if '_debug' in x else ""
+        new_name = names[x] if x in names.keys() else x
+        return new_name + suffix
+
+    df.loc[:, 'measure'] = df.measure.apply(rename)
     if len(df.n_preds.unique()) > 1:
         df.measure += df.n_preds.apply(lambda x: f" (n={x})")
 
     # Compute a suitable size for the heatmaps
     height = 4 + round(len(df['predictor'].unique())/4)
 
-    def _draw_heatmap(*args, **kwargs):
-        """ Draws a heatmap in a FacetGrid with custom parameters
-        """
-
-        df = kwargs.pop('data')
-        annot = kwargs.pop('annotate')
-
-        types = df["type"].unique()
-        df.index.name = 'predictor'
-        df.columns.name = 'predicted'
-        df = df.pivot(index=args[0], columns=args[1], values=args[2])
-
-        # For n_pairs, we want a specific set of parameters.
-        if 'Number of pairs' in types:
-            hm_cmap = "gray_r"
-            annot = df.shape[0] < 10
-            fmt = ".0f"
-        else:
-            hm_cmap = cmap
-            fmt = ".2f"
-
-        if feat_order:
-
-            # Sorting for multiple predictors.
-            def sorting(x):
-                return x.apply(lambda cell: feat_order.index(cell))
-
-            sort = df.index.to_frame().predictor.str.split('&', expand=True)
-            sort = sort.sort_values(by=list(sort.columns), key=sorting, axis=0)
-            df = df.reindex(sort.index)
-            df = df[feat_order]
-
-        else:
-            df = df.reindex(list(df.columns))
-
-        df = df.replace([np.nan], 0)
-
-        # Additional options for rendering.
-        # Extra short labels
-        if dense:
-            def shorten(x):
-                return ".".join([f[0].capitalize() for f in x.split('.')])
-
-            df.index = pd.Index(df.index.to_series().map(shorten))
-            df.columns = pd.Index(df.columns.to_series().map(shorten))
-
-        # Annotations on the heatmap
-        if annot:
-            annot = df.copy().round(2)
-
-        # Mask (diagonal)
-        df_m = pd.DataFrame(columns=df.columns, index=df.index)
-        for col in df.columns:
-            tmp = df_m.index.to_frame().predictor.str.split('&').apply(lambda x: col in x)
-            df_m.loc[tmp, [col]] = True
-
-        # Drawing each individual heatmap
-        sns.heatmap(df,
-                    annot=annot,
-                    mask=df_m,
-                    cmap=hm_cmap,
-                    fmt=fmt,
-                    linewidths=1,
-                    **kwargs)
-
     # Plotting the heatmap
-    cg = sns.FacetGrid(df, row='measure', col='type', height=height, margin_titles=True,
+    cg = sns.FacetGrid(df, **orient_param,
+                       height=height, margin_titles=True,
                        sharex=False, sharey=False)
     cg.set_titles(row_template='{row_name}', col_template='{col_name}')
 
-    cg.map_dataframe(_draw_heatmap, 'predictor', 'predicted', 'value',
-                     annotate=annotate, square=True, cbar=True,
+    cg.map_dataframe(_draw_heatmap, 'predictor', 'predicted', 'value', 'reverse',
+                     annotate=annotate, cmap=cmap,
+                     dense=dense, square=True, cbar=True,
                      cbar_kws=dict(location='bottom',
                                    shrink=0.6,
-                                   pad=0.075))  # Spacing between colorbar and hm
+                                   pad=0.075),  # Spacing between colorbar and hm
+                     **kwargs)
 
     # Setting labels
     rotate = 0 if dense else 90
@@ -192,7 +217,6 @@ def entropy_heatmap(results, md, cmap_name=False,
                              "content": "figure"})
 
     log.info("Writing heatmap to: " + name)
-
     cg.savefig(name, pad_inches=0.1)
 
 
@@ -213,4 +237,5 @@ def ent_heatmap_command(cfg, md):
                     cmap_name=cfg.heatmap.cmap,
                     feat_order=feat_order,
                     dense=cfg.heatmap.dense,
-                    annotate=cfg.heatmap.annotate)
+                    annotate=cfg.heatmap.annotate,
+                    n_pairs=cfg.heatmap.n_pairs)
