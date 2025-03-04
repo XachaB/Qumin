55f6024f1eb4e3e9b84ddec1f2a3d629d82bc987
Sacha Beniamine <sacha@git.none>
2025-03-04 12:17:12 +0000

    hotfix: cells => cfg.cells

--- src/qumin/calc_paradigm_entropy.py
+++ src/qumin/calc_paradigm_entropy.py
@@ -49,7 +49,7 @@ def H_command(cfg, md):
     patterns = ParadigmPatterns()
     patterns.from_file(patterns_folder_path,
                        paradigms.data,
-                       cells=cells,
+                       cells=cfg.cells,
                        defective=defective,
                        overabundant=False,
                        force=cfg.force,
